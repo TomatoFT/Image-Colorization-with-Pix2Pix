5101c7faff843ab82b5309d9b58767224e1aa032
TomatoFT <20520926@gm.uit.edu.vn>
2024-04-15 01:42:51 +0700

    Add the Deoldify section for demo page

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 import sys
 
 sys.path.append("./methods/Pix2Pix")
+sys.path.append("./methods/GFPGAN")
+sys.path.append("./methods/Deoldify")
 
 import cv2
 import gradio as gr
@@ -12,6 +14,7 @@ from torchvision import transforms
 from methods.Pix2Pix.config.constants import MEAN, STD
 from methods.Pix2Pix.dataloader import Transform
 from methods.Pix2Pix.train import load_model
+from methods.DeOldify.inference import get_the_image_restoration
 
 # Define the Generator and Discriminator
 
@@ -21,17 +24,6 @@ def de_norm(img):
     img_ = np.transpose(img_, (1, 2, 0))
     return img_
 
-
-def sharpen_image(image):
-    # Define a sharpening kernel
-    kernel = np.array([[-1, -1, -1],
-                       [-1,  9, -1],
-                       [-1, -1, -1]])
-
-    # Apply the sharpening filter
-    sharpened_image = cv2.filter2D(image, -1, kernel)
-
-    return sharpened_image
 
 def post_process_sharpen(image):
     blurred = cv2.GaussianBlur(image, (0, 0), 3)
@@ -43,30 +35,38 @@ def post_process_sharpen(image):
 generator = load_model('45', demo=True)  # Replace with the path to your model weights
 generator.eval()
 
-def colorize_image(input_image):
+def colorize_image(input_image, method):
     # Convert Grayscale to RGB
     transformer = Transform()
     
     # Ensure the input image has the correct data type (uint8)
     img = Image.fromarray(np.array(input_image, dtype=np.uint8))
-    original_size = img.size
+        # Generate colorized image based on selected method
+    if method == 'Pix2Pix':
+        original_size = img.size
 
-    # Transform input image
-    input_tensor = transformer(img)
-    input_tensor = torch.unsqueeze(input_tensor, 0)
+        # Transform input image
+        input_tensor = transformer(img)
+        input_tensor = torch.unsqueeze(input_tensor, 0)
 
-    # Generate colorized image
-    with torch.no_grad():
-        colorized_tensor = generator(input_tensor)
+        # Generate colorized image
+        with torch.no_grad():
+            colorized_tensor = generator(input_tensor)
 
-    colorized_image = de_norm(colorized_tensor[0].cpu())
+        colorized_image = de_norm(colorized_tensor[0].cpu())
 
-    return post_process_sharpen(cv2.resize(colorized_image, (original_size[0], original_size[1])))
+        return post_process_sharpen(cv2.resize(colorized_image, (original_size[0], original_size[1])))
+    
+    elif method == 'GFPGAN':
+        # _ = get_the_image_restoration(choice="Uploaded", path=file, output=generated_img_folder)
+        pass
+    else:
+        raise ValueError("Invalid method selected. Please choose either 'Pix2Pix' or 'GFPGAN'.")
 
 # Gradio Interface
 iface = gr.Interface(
     fn=colorize_image,
-    inputs="image",
+    inputs=["image", gr.inputs.Dropdown(["Pix2Pix", "GFPGAN"], label="Method")],
     outputs="image",
     live=True,
     title="Image Colorization Demo",
